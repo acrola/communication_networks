22d71c6dc684cf28053772346519a244268e852a
noamgot <noam.got+github@gmail.com>
2016-11-11 13:43:22 +0200

    this is a test #2

--- HW1/main.c
+++ HW1/main.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-    printf("hello world");
-    printf("this is a test");
-    printf("editing in github to see if pull works");
+    printf("hello world\n");
+    printf("this is a test\n");
+    printf("editing in github to see if pull works\n");
     return 0;
 }
 
