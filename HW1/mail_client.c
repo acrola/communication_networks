f7df6f5cfe043dcc1912a0b91dc7e1a84b1b1f64
Noam G <noam.got+git@gmail.com>
2016-11-17 19:12:45 +0200

    HUGE UPDATE!  completed the connection process of the client - including initial connection  establishment, getting username and password from user and validating them  with the server

--- HW1/mail_client.c
+++ HW1/mail_client.c
@@ -6,9 +6,11 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#include <unistd.h>
 #include <errno.h>
 #include <ctype.h>
+#include <unistd.h>
+
+#define BUF_SIZE 256
 
 typedef enum _eOpCode
 {
@@ -26,51 +28,34 @@ int sendall(int sock, char *buf, int *len);
 
 int sendMessage(int sock, unsigned int operation, unsigned short mail_id);
 
-int connectToHostname(int sock, char *hostname, char *port);
+int connectToServer(int sockfd, char *hostname, char *port);
 
 void tryClose(int sock);
 
 int recvall(int sock, char *buf, int *len);
 
+void analyzeProgramArguments(int argc, char *argv[], char **hostname, char **port);
+
+void getAndSendUserDetails(int sockfd);
+
 int main(int argc, char *argv[])
 {
-    int sock;
-    char *hostname;
-    char *port;
+    int sockfd;
+    char *hostname, *port;
 
-    /*exit in case of incorrect argument count*/
-    if (argc > 3)
-    {
-        printf("Incorrect Argument Count");
-        exit(EXIT_FAILURE);
-    }
-    /* place default hostname if required*/
-    if (argc == 1)
-    {
-        hostname = "localhost";
-    }
-    else
-    {
-        hostname = argv[1];
-    }
-
-    /* place default port if required*/
-    if (argc < 3)
-    {
-        port = "6423";
-    }
-    else
-    {
-        port = argv[2];
-    }
-
+    analyzeProgramArguments(argc, argv, &hostname, &port);
 
     /* open TCP socket with IPv4*/
-    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
     {
-        printf("error in opening client's socket: %s", strerror(errno));
+        perror("Error in opening client's socket");
         exit(EXIT_FAILURE);
     }
+
+    /* connect to server*/
+    connectToServer(sockfd, hostname, port);
+
+
 
     /* get correct input from user*/
     int inputDone = 0;
@@ -87,13 +72,12 @@ int main(int argc, char *argv[])
         inputDone = 1;
     }
 
-    /* connect to server*/
-    connectToHostname(sock, hostname, port);
+
 
     /* TODO - after connection was established succesfully, get input operations from user, parse correctly and use "sendMessage" function to send to server*/
     /* TODO - complete reading something from server*/
-    while (((recvall(sock, (char *) &buff, &len1)) == 0) &&
-           ((recvall(sock, (char *) &nums, &len2)) == 0))
+    while (((recvall(sockfd, (char *) &buff, &len1)) == 0) &&
+           ((recvall(sockfd, (char *) &nums, &len2)) == 0))
     {
         int num;
         int inputDone = 0;
@@ -102,7 +86,7 @@ int main(int argc, char *argv[])
         if (len1 != 1 || len2 != 4)
         {
             perror("Error receiving data from server");
-            tryClose(sock);
+            tryClose(sockfd);
             exit(errno);
         }
 
@@ -110,7 +94,7 @@ int main(int argc, char *argv[])
         /* if number is not in range, send 1023 to make move invalid*/
         n = (num > 0 && num <= 1000) ? (unsigned short) num : 0x3FF;
         /* try to send the message*/
-        if (sendMessage(sock, c, n) < 0)
+        if (sendMessage(sockfd, c, n) < 0)
         {
             perror("Error sending message");
             break;
@@ -126,13 +110,14 @@ int main(int argc, char *argv[])
             perror("Error receiving message");
         }
     }
-    tryClose(sock);
+    tryClose(sockfd);
     return 0;
 }
 
 int sendMessage(int sock, unsigned int operation, unsigned short mail_id)
 {
     /* TODO - implement with protocol I did in the server*/
+    return 0;
 }
 
 int sendall(int sock, char *buf, int *len)
@@ -140,10 +125,10 @@ int sendall(int sock, char *buf, int *len)
     /* sendall code as seen in recitation*/
     int total = 0; /* how many bytes we've sent */
     int bytesleft = *len; /* how many we have left to send */
-    int n;
+    int n = 0;
     while (total < *len)
     {
-        n = send(sock, buf + total, bytesleft, 0);
+        n = send(sock, buf + total, (size_t) bytesleft, 0);
         if (n == -1)
         {
             break;
@@ -155,73 +140,108 @@ int sendall(int sock, char *buf, int *len)
     return n == -1 ? -1 : 0; /*-1 on failure, 0 on success */
 }
 
-int connectToHostname(int sock, char *hostname, char *port)
+int connectToServer(int sockfd, char *hostname, char *port)
 {
-    const char separator[2] = " ";
-    char *token;
 
-    char username_line[256];
-    char *username;
-    if (!fgets(user_line, sizeof(user_line), stdin))
-    {
-        printf("Error reading username, probably too long.\n");
-        exit(1);
-    }
-    token = strtok(user_line, separator);
-    username = strtok(NULL, separator);
 
-    char password_line[256];
-    char *password;
-    if (!fgets(password_line, sizeof(password_line), stdin))
-    {
-        printf("Error reading password, probably too long.\n");
-        exit(1);
-    }
-    token = strtok(password_line, separator);
-    password = strtok(NULL, separator);
+
+    //struct sockaddr_in *h;
+    //struct sockaddr_in dest_addr;
+
+//    /* server addr is IPv4*/
+//    dest_addr.sin_family = AF_INET;
+//    /* write server port*/
+//    dest_addr.sin_port = htons((short) strtol(port, NULL, 10));
+
 
     int rv;
     struct addrinfo hints, *servinfo, *p;
-    struct sockaddr_in *h;
-    struct sockaddr_in dest_addr;
+    char buf[BUF_SIZE] = {0};
 
-    /* server addr is IPv4*/
-    dest_addr.sin_family = AF_INET;
-    /* write server port*/
-    dest_addr.sin_port = htons((short) strtol(port, NULL, 10));
-    /* zero out hints (since garbage is bad)*/
+
+    /* zero hints struct*/
     memset(&hints, 0, sizeof(hints));
     /* we want IPv4 address and TCP*/
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
+
     /* try get address info*/
     if ((rv = getaddrinfo(hostname, port, &hints, &servinfo)) != 0)
     {
         fprintf(stderr, "Error getting address info: %s\n", gai_strerror(rv));
-        exit(1);
+        exit(EXIT_FAILURE);
     }
     p = servinfo;
     while (p != NULL)
     {
         /* put last address into sent pointer*/
-        h = (struct sockaddr_in *) p->ai_addr;
-        dest_addr.sin_addr = h->sin_addr;
-        if (connect(sock, (struct sockaddr *) (&dest_addr), sizeof(struct sockaddr)) == 0)
+        //h = (struct sockaddr_in *) p->ai_addr;
+        //dest_addr.sin_addr = h->sin_addr;
+
+
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == 0)
         {
-            break;
-        }/* if connection succesfull*/
+            break; //connection established
+        }
+
         p = p->ai_next;
     }
-    freeaddrinfo(servinfo);
-    if (p == NULL)
+
+    if (p == NULL) //We didn't find a host
     {
-        /*We didnt find a host*/
-        tryClose(sock);
-        perror("Could not connect to server");
-        exit(errno);
+        tryClose(sockfd);
+        fprintf(stderr, "Could not connect to server");
+        exit(EXIT_FAILURE);
     }
+    /*free the servinfo struct - we're done with it"*/
+    freeaddrinfo(servinfo);
+
+    /* The initial connection has been established.
+     * The client needs to recieve the server's welcome message:*/
+    //todo - needs to be changed. not robust enouge (what if the welcome message is changed?)
+    int msg_length = sizeof(char) * strlen("Welcome! I am simple-mail-server.\n");
+    recvall(sockfd, buf, &msg_length);
+    printf("%s", buf);
+
+    /*now the client takes input from the user and sends it to the server:*/
+    getAndSendUserDetails(sockfd);
+
+
     printf("Connected to server\n");
     return 0;
+}
+
+void getAndSendUserDetails(int sockfd)
+{
+    const char delimiters[] = " \t\n";
+    char *token, input[BUF_SIZE] = {0}, username[BUF_SIZE] = {0}, password[BUF_SIZE] = {0};
+    char sendBuf[(BUF_SIZE*2)+1];
+
+    printf("User: ");
+    if (!fgets(input, BUF_SIZE, stdin))
+    {
+        printf("Error - reading username failed\n");
+        exit(EXIT_FAILURE);
+    }
+    token = strtok(input, delimiters);
+    strcpy(username, token);
+
+    memset(input, 0, BUF_SIZE);
+    printf("Password: ");
+    if (!fgets(input, BUF_SIZE, stdin))
+    {
+        printf("Error - reading password failed\n");
+        exit(EXIT_FAILURE);
+    }
+    token = strtok(input, delimiters);
+    strcpy(password, token);
+    //copy the data to sendBuf in the form of <username>\t<password>
+    strcpy(sendBuf, username);
+    strcat(sendBuf, "\t");
+    strcat(sendBuf, password);
+    int bytesToSend = strlen(sendBuf);
+    //send the usernameand password
+    sendall(sockfd, sendBuf, &bytesToSend);
 }
 
 void tryClose(int sock)
@@ -281,3 +301,51 @@ void shutdownSocket(int sock)
     tryClose(sock);
     exit(0);
 }
+
+void analyzeProgramArguments(int argc, char **argv, char **hostname, char **port)
+{
+    /*exit in case of incorrect argument count*/
+    if (argc > 3)
+    {
+        printf("Incorrect Argument Count");
+        exit(EXIT_FAILURE);
+    }
+    /* place default hostname if required*/
+    if (argc == 1)
+    {
+        *hostname = "localhost";
+    }
+    else
+    {
+        *hostname = argv[1];
+    }
+
+    /* place default port if required*/
+    if (argc < 3)
+    {
+        *port = "6423";
+    }
+    else
+    {
+        *port = argv[2];
+    }
+
+    /*
+    else
+    {
+        //TODO - needs code review to see if we really catch everything..
+        char *temp;
+        long tempPort = strtol(argv[2], &temp, 10);
+        //check if error occurred during the conversion
+        if (temp == argv[2] || *temp != '\0' || ((tempPort == LONG_MIN || tempPort == LONG_MAX)&& errno == ERANGE))
+        {
+            printf("Invalid port number error");
+            exit(EXIT_FAILURE);
+        }
+        *port = (short)tempPort;
+    }*/
+
+}
+
+
+
